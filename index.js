c12c3caa7d7d69528f4fe5ecf2abcaee7297a379
npoplove <poplove.noah@gmail.com>
2022-03-05 17:09:48 -0500

    Access and refresh tokens

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2').Strategy;
+
+let access_token = null;
+let refresh_token = null;
+let count = 0;
+
+const redirectUri = "https://a9f3-142-126-105-252.ngrok.io/oauth-callback";
+const serverAddr = encodeURI(redirectUri);
+const redirectUri2 = "https://a9f3-142-126-105-252.ngrok.io";
+const serverAddr2 = encodeURI(redirectUri2);
+
+const altAddr = 'https://apple.com'
+
+passport.use(new OAuth2Strategy({
+  authorizationURL: 'https://api.login.yahoo.com/oauth2/request_auth',
+  tokenURL: 'https://api.login.yahoo.com/oauth2/get_token',
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "https://a9f3-142-126-105-252.ngrok.io/oauth-callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log("reached here :)");
+  console.log("access token: " + accessToken);
+  console.log("refresh token: " + refreshToken);
+}
+));
 
 const app = express();
 
@@ -11,17 +38,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
-const redirectUri = "https://75a1-142-126-105-252.ngrok.io/oauth-callback/";
-const redirectUri2 = "https://75a1-142-126-105-252.ngrok.io/";
-const serverAddr = encodeURI(redirectUri);
-console.log(redirectUri);
-console.log(serverAddr);
-
-
 const redirectAddress = 
 `https://api.login.yahoo.com/oauth2/request_auth?client_id=${process.env.CLIENT_ID}&redirect_uri=${serverAddr}&response_type=code&language=en-us`;
 
-const encodedclientId = Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET).toString('base64');
+const encodedclientId = Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET).toString("base64");
+
+app.get('/auth',
+  passport.authenticate('oauth2'));
+
+app.get('/oauth-callback',
+  passport.authenticate('oauth2', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log("success?");
+    // Successful authentication, redirect home.
+    res.redirect('/?what=1');
+  });
 
 app.get('/auth', (req, res) => {
   console.log("app.get auth");
@@ -33,28 +64,29 @@ app.get('/oauth-callback', ({ query: { code } }, res) => {
   const body = {
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
-    redirect_uri: redirectUri2,
+    redirect_uri: altAddr,
     code,
-    grant_type: "authorization_code"
+    grant_type: "authorization_code",
   };
   const opts = { headers: { 
     Authorization: `Basic ${encodedclientId}`,
-    "Content-Type": "application/x-www-form-urlencoded",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
-  
+    "Content-Type": "application/x-www-form-urlencoded",  
   } };
   console.log("app.get auth-callback + axios");
-  console.log(body);
+  //console.log(opts);
+  //console.log(body);
+  console.log(count);
+  console.log(access_token)
+  count ++;
   axios
     .post('https://api.login.yahoo.com/oauth2/get_token', body, opts)
-    .then((_res) => _res.data.access_token)
-    .then((token) => {
-      // eslint-disable-next-line no-console
-      console.log('My token:', token);
-
-      res.redirect(`/?token=${token}`);
-    })
-    .catch((err) => res.status(500).json({ err: err.message }));
+    .then((_res) => {
+      console.log("roken:");
+      console.log(_res);
+    }, (error) => {
+      console.log("callback error :(");
+      console.log(error);
+    });
 });
 
 app.listen(3000);
